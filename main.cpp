c3074b1019476991187e4a7507608d99d848ab36
Paul Masri-Stone <paulmasri@users.noreply.github.com>
2023-12-12 15:12:57 +0000

    Use UWP app data location, where possible, instead of QStandardPaths::AppDataLocation for output log

--- main.cpp
+++ main.cpp
@@ -9,7 +9,22 @@
 #include <QDebug>
 #include <QDir>
 
+#include <winrt/Windows.Storage.h>
+#include <winrt/Windows.Foundation.h>
+
+QString getUwpAppDataLocation() {
+    try {
+        auto localFolder = winrt::Windows::Storage::ApplicationData::Current().LocalFolder();
+        std::wstring folderPath = localFolder.Path().c_str();
+        return QString::fromStdWString(folderPath);
+    } catch (const std::exception& e) {
+        qWarning() << "Failed to get the Qt 5 AppData path:" << e.what();
+        return QString();
+    }
+}
+
 QString logFileTimestamp;
+QString appDataLocation;
 
 void customMessageHandler(QtMsgType type, const QMessageLogContext &context, const QString &msg)
 {
@@ -21,7 +36,7 @@ void customMessageHandler(QtMsgType type, const QMessageLogContext &context, con
                                .arg(context.line)
                                .arg(context.function);
 
-    QString dirPath = QStandardPaths::writableLocation(QStandardPaths::AppLocalDataLocation) + "/Qt6-CMake-QML";
+    QString dirPath = appDataLocation + "/Qt6-CMake-QML";
     QDir dir(dirPath);
     if (!dir.exists()) {
         dir.mkpath(".");
@@ -40,6 +55,10 @@ int main(int argc, char* argv[])
 
     // Generate the timestamp once when the application starts
     logFileTimestamp = QDateTime::currentDateTime().toString("yyyy-MM-dd_hh-mm-ss");
+
+    appDataLocation = getUwpAppDataLocation();
+    if (appDataLocation.isEmpty())
+        appDataLocation = QStandardPaths::writableLocation(QStandardPaths::AppDataLocation);
 
     // Set up the custom message handler
     qInstallMessageHandler(customMessageHandler);
